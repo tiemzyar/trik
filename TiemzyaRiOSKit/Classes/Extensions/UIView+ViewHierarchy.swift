b94b18e789f0b59778ec5ee51646f8fa66fef61e
tiemzyar <52572379+tiemzyar@users.noreply.github.com>
2021-12-19 10:18:21 +0100

    UIView view hierarchy extension: Refactored method for getting subviews recursively

--- TiemzyaRiOSKit/Classes/Extensions/UIView+ViewHierarchy.swift
+++ TiemzyaRiOSKit/Classes/Extensions/UIView+ViewHierarchy.swift
@@ -2,8 +2,8 @@
 //  UIView+ViewHierarchy.swift
 //  TiemzyaRiOSKit
 //
-//  Created by Manuel Zhou on 12.07.20.
-//  Copyright © 2020 tiemzyar.
+//  Created by tiemzyar on 12.07.20.
+//  Copyright © 2020-2021 tiemzyar.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
@@ -29,20 +29,10 @@ import UIKit
 
 // MARK: -
 // MARK: View Hierarchy
+/*
+Extension of UIView with functionality related to the view hierarchy of a view.
+*/
 public extension UIView {
-	// MARK: Nested types
-
-
-	// MARK: Type properties
-
-
-	// MARK: Type methods
-
-
-	// MARK: -
-	// MARK: Instance properties
-
-
 	// MARK: -
 	// MARK: Instance methods
 	/**
@@ -62,13 +52,31 @@ public extension UIView {
 	
 	/**
 	Recursively searches through the view's subview hierarchy to find all subviews of type `type`.
+	 
+	This method is deprecated and will be removed in a future version of this framework.
 	
 	- parameters:
 		- type: Type of subviews to find
 	
 	- returns: Array of found subviews
 	*/
+	@available(*, deprecated, message: "Use 'getSubview(ofType:recursive:)' instead")
 	func getSubviewsRecursive<T : UIView>(ofType type: T.Type) -> [T] {
+		return self.getSubviews(ofType: type, recursive: true)
+	}
+	
+	/**
+	Searches through the view's direct subviews to find all subviews of a given type.
+	
+	If `recursive` is true, this method will perform its search recursively through the view's entire subview hierarchy.
+	
+	- parameters:
+		- type: Type of subviews to find
+		- recursive: Flag about whether to search in entire subview hierarchy
+	
+	- returns: Array of found subviews
+	*/
+	func getSubviews<T : UIView>(ofType type: T.Type, recursive: Bool = false) -> [T] {
 		var subviews = [T]()
 
 		for subview in self.subviews {
@@ -76,7 +84,9 @@ public extension UIView {
 				subviews.append(sv)
 			}
 			
-			subviews += subview.getSubviewsRecursive(ofType: type)
+			if recursive {
+				subviews += subview.getSubviews(ofType: type, recursive: true)
+			}
 		}
 
 		return subviews
